9d0c35454f3d09748434226d24612c7eec08db27
Maureen Ononiwu <59079323+Chinwendu20@users.noreply.github.com>
2022-03-24 14:54:52 +0100

    modified Procfile

--- DpGenerator/temp.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-from PIL import Image,ImageDraw
-from Photo.models import Post
-
-urllib.request.urlretrieve(Post.objects.get(id=1).Banner.url, "Banner")
-Image.open('Banner')
